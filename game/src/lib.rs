51e8316b0485a402c4d737a1463ada9d107fd802
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2024-09-12 14:35:22 -0700

    cleanups

--- game/src/lib.rs
+++ game/src/lib.rs
@@ -1,6 +1,9 @@
 use crate::player::Player;
 use fyrox::{
-    core::{notify::EventKind, pool::Handle, reflect::prelude::*, uuid, visitor::prelude::*},
+    core::{
+        algebra::Vector2, notify::EventKind, pool::Handle, reflect::prelude::*, uuid,
+        visitor::prelude::*,
+    },
     engine::GraphicsContext,
     event::{ElementState, Event, WindowEvent},
     graph::SceneGraph,
@@ -18,7 +21,11 @@ use fyrox::{
     window::CursorGrabMode,
 };
 
-use std::{path::Path, thread, time};
+use std::{
+    path::Path,
+    thread,
+    time::{self, Duration, Instant, SystemTime, UNIX_EPOCH},
+};
 
 pub mod player;
 // pub mod weapon;
@@ -35,25 +42,39 @@ pub struct Game {
     #[reflect(hidden)]
     cursor_released: bool,
 
-    quit_button_handle: Handle<UiNode>,
+    forward_button: Handle<UiNode>,
+    backward_button: Handle<UiNode>,
+
     player: Handle<Node>,
+
+    secs: f64,
 }
 
-fn create_quit_button(ui: &mut UserInterface) -> Handle<UiNode> {
-    ButtonBuilder::new(WidgetBuilder::new().with_width(100.0).with_height(100.0))
-        .with_content(
-            TextBuilder::new(WidgetBuilder::new())
-                .with_text("forward")
-                .build(&mut ui.build_ctx()),
-        )
-        .build(&mut ui.build_ctx())
+#[derive(Debug)]
+enum Direction {
+    Forward,
+    Back,
+    Empty,
 }
 
-// impl Game {
-//     fn init(&mut self, scene_path: Option<&str>, context: PluginContext) {
-//         self.quit_button_handle =  create_quit_button(context.user_interfaces.first_mut());
-//     }
+// fn timer() -> f64 {
+//     SystemTime::now()
+//         .duration_since(UNIX_EPOCH)
+//         .unwrap()
+//         .as_secs_f64()
 // }
+
+impl Game {
+    fn direction(&self, dest: Handle<UiNode>) -> Direction {
+        if dest == self.forward_button {
+            return Direction::Forward;
+        }
+        if dest == self.backward_button {
+            return Direction::Back;
+        }
+        return Direction::Empty;
+    }
+}
 
 impl Plugin for Game {
     fn on_graphics_context_initialized(&mut self, context: PluginContext) {
@@ -73,21 +94,41 @@ impl Plugin for Game {
         let c = &context.serialization_context.script_constructors;
 
         c.add::<Player>("Player");
-
-        let u = uuid!("62ae8f72-896e-412d-843c-3e24540e7f38");
-        let Some(s) = c.try_create(&u) else {
-            return;
-        };
-        println!("script: {:#?}", s);
-        let Some(p) = s.cast::<Player>() else {
-            return;
-        };
-        println!("Got player: {:#?}", p);
     }
 
     fn init(&mut self, scene_path: Option<&str>, context: PluginContext) {
         // println!("game init");
-        self.quit_button_handle = create_quit_button(context.user_interfaces.first_mut());
+        // self.quit_button_handle = create_quit_button(context.user_interfaces.first_mut());
+        let ui = context.user_interfaces.first_mut();
+        let ctx = &mut ui.build_ctx();
+        // self.forward_button = ButtonBuilder::new(
+        //     WidgetBuilder::new()
+        //         .with_width(400.0)
+        //         .with_height(400.0)
+        //         .with_desired_position(Vector2::new(50.0, 300.0)),
+        // )
+        // .with_repeat_clicks_on_hold(true)
+        // .with_content(
+        //     TextBuilder::new(WidgetBuilder::new())
+        //         .with_text("forward")
+        //         .build(ctx),
+        // )
+        // .build(ctx);
+
+        // self.backward_button = ButtonBuilder::new(
+        //     WidgetBuilder::new()
+        //         .with_width(400.0)
+        //         .with_height(400.0)
+        //         .with_desired_position(Vector2::new(50.0, 700.0)),
+        // )
+        // .with_repeat_clicks_on_hold(true)
+        // .with_content(
+        //     TextBuilder::new(WidgetBuilder::new())
+        //         .with_text("backwards")
+        //         .build(ctx),
+        // )
+        // .build(ctx);
+
         context
             .async_scene_loader
             .request(scene_path.unwrap_or("data/scene.rgs"));
@@ -97,8 +138,24 @@ impl Plugin for Game {
         // Do a cleanup here.
     }
 
-    fn update(&mut self, _context: &mut PluginContext) {
+    fn update(&mut self, context: &mut PluginContext) {
         // Add your global update code here.
+        if let Some(scene) = context.scenes.try_get_mut(self.scene) {
+            let p = scene
+                .graph
+                .try_get_script_of_mut::<Player>(self.player);
+            if p.is_none(){
+            return
+            }
+            let p = p.unwrap();
+            // let now = timer();
+            // let diff = now - self.secs;
+            // println!("diff {}", diff);
+            // if diff > 0.12 {
+            //     p.move_backward = false;
+            //     p.move_forward = false;
+            // }
+        }
     }
 
     fn on_os_event(&mut self, event: &Event<()>, context: PluginContext) {
@@ -140,21 +197,58 @@ impl Plugin for Game {
     }
 
     fn on_ui_message(&mut self, context: &mut PluginContext, message: &UiMessage) {
-        println!("data {:#?}", message);
         if let Some(scene) = context.scenes.try_get_mut(self.scene) {
-            let p = scene.graph.try_get_script_of_mut::<Player>(self.player).unwrap();
-            // println!("{:#?}",p);
-            println!("DT: {}",context.dt);
-            // p.move_forward = true;
+            let p = scene
+                .graph
+                .try_get_script_of_mut::<Player>(self.player)
+                .unwrap();
 
+            let dest = message.destination();
+            let d = self.direction(dest);
+
+            // println!("DT: {:#?}", context.dt);
+            match d {
+                Direction::Forward => p.move_forward = true,
+                Direction::Back => p.move_backward = true,
+                _ => return,
+            };
+            println!("DIRECTION: {:#?}; {:#?}", d, message);
+            // let now = timer();
+            // let diff = now-self.secs;
+
+            // let mut sleeping = false;
+            // self.secs = timer();
+            context.task_pool.spawn_plugin_task(
+                async move {
+                    // p.move_forward = false;
+                    // p.move_backward = false;
+                    let ten_millis = time::Duration::from_millis(10);
+                    // sleeping = true;
+                    // thread::sleep(ten_millis);
+                },
+                move |_, s: &mut Game, c| {
+                    let scene = c.scenes.try_get_mut(s.scene).unwrap();
+                    let p = scene
+                        .graph
+                        .try_get_script_of_mut::<Player>(s.player)
+                        .unwrap();
+                    // let now = timer();
+                    // println!("dur: {}",now-s.secs);
+                    // if now - s.secs >= 0.1 {
+                    //     println!("stopping");
+                    //     match d {
+                    //         Direction::Forward => p.move_forward = false,
+                    //         Direction::Back => p.move_backward = false,
+                    //         _ => return,
+                    //     };
+                    //     // p.move_backward = false;
+                    //     // p.move_forward = false;
+
+                    // }
+                },
+            );
             // p.move_forward = false;
-            if let Some(WidgetMessage::MouseDown { pos, button }) = message.data() {
-                println!("MOUSE DOWN");
-                p.move_forward = true;
-            }
-            if let Some(WidgetMessage::MouseUp { pos, button }) = message.data(){
-                p.move_forward = false;
-            }
+            // p.move_backward = false;
         }
     }
 
@@ -172,5 +266,26 @@ impl Plugin for Game {
         _context: &mut PluginContext,
     ) {
         self.scene = scene;
+    }
+
+    fn register_property_editors(
+        &self,
+    ) -> fyrox::gui::inspector::editors::PropertyEditorDefinitionContainer {
+        fyrox::gui::inspector::editors::PropertyEditorDefinitionContainer::empty()
+    }
+
+    fn on_loaded(&mut self, #[allow(unused_variables)] context: PluginContext) {}
+
+    fn before_rendering(&mut self, #[allow(unused_variables)] context: PluginContext) {}
+
+    fn on_graphics_context_destroyed(&mut self, #[allow(unused_variables)] context: PluginContext) {
+    }
+
+    fn on_scene_loading_failed(
+        &mut self,
+        #[allow(unused_variables)] path: &Path,
+        #[allow(unused_variables)] error: &VisitError,
+        #[allow(unused_variables)] context: &mut PluginContext,
+    ) {
     }
 }
--- game/src/player.rs
+++ game/src/player.rs
@@ -1,6 +1,8 @@
+use fyrox::core::algebra::{Matrix4, Point3};
+use fyrox::core::math::frustum::Frustum;
 use fyrox::core::reflect::GetField;
 use fyrox::generic_animation::machine::Parameter;
-use fyrox::graph::SceneGraph;
+use fyrox::graph::{BaseSceneGraph, SceneGraph};
 use fyrox::gui::button::{ButtonBuilder, ButtonMessage};
 use fyrox::gui::message::UiMessage;
 use fyrox::gui::text::TextBuilder;
@@ -8,6 +10,7 @@ use fyrox::gui::widget::WidgetBuilder;
 use fyrox::gui::{UiNode, UserInterface};
 use fyrox::plugin::{Plugin, PluginContext};
 use fyrox::scene::animation::absm::AnimationBlendingStateMachine;
+use fyrox::scene::graph::Graph;
 use fyrox::window::Window;
 use fyrox::{
     core::{
@@ -37,7 +40,7 @@ pub struct Player {
 
     #[visit(optional)]
     #[reflect(hidden)]
-    move_backward: bool,
+    pub move_backward: bool,
 
     #[visit(optional)]
     #[reflect(hidden)]
@@ -53,6 +56,10 @@ pub struct Player {
 
     #[visit(optional)]
     #[reflect(hidden)]
+    fight: bool,
+
+    #[visit(optional)]
+    #[reflect(hidden)]
     yaw: f32,
 
     #[visit(optional)]
@@ -65,8 +72,21 @@ pub struct Player {
     #[visit(optional)]
     camera: Handle<Node>,
 
+    #[visit(skip)]
+    #[reflect(hidden)]
+    frustum: Frustum,
+
+    // #[visit(skip)]
+    // #[reflect(hidden)]
+    target: Handle<Node>,
+
     quit_button_handle: Handle<UiNode>,
 }
+
+fn handle_to_node(graph: &mut Graph, handle: Handle<Node>) -> &mut Node {
+    graph.try_get_mut(handle).unwrap()
+}
+
 fn create_quit_button(ui: &mut UserInterface) -> Handle<UiNode> {
     ButtonBuilder::new(WidgetBuilder::new())
         .with_content(
@@ -77,15 +97,43 @@ fn create_quit_button(ui: &mut UserInterface) -> Handle<UiNode> {
         .build(&mut ui.build_ctx())
 }
 
+impl Player {
+    fn update_frustum(
+        &mut self,
+        position: Vector3<f32>,
+        look_vector: Vector3<f32>,
+        up_vector: Vector3<f32>,
+        max_observing_distance: f32,
+    ) {
+        // Calculate an average head position.
+        let head_pos = position + Vector3::new(0.0, 0.4, 0.0);
+        // let look_at = head_pos;// + look_vector;
 
+        // View matrix is constructed using three parameters - observer position, target point,
+        // and an up vector (usually it is just (0,1,0) vector).
+        // let view_matrix =
+        //     Matrix4::look_at_rh(&Point3::from(head_pos), &Point3::from(look_at), &up_vector);
+
+        // Build the perspective projection matrix.
+        let projection_matrix = Matrix4::new_perspective(
+            // Aspect ratio
+            16.0 / 9.0,
+            // Field of view of the bot
+            90.0f32.to_radians(),
+            0.1,
+            max_observing_distance,
+        );
+        // self.frustum =
+        //     Frustum::from_view_projection_matrix(projection_matrix * view_matrix).unwrap();
+        self.frustum = Frustum::from_view_projection_matrix(projection_matrix).unwrap();
+    }
+}
 
 impl ScriptTrait for Player {
-
     fn on_init(&mut self, context: &mut ScriptContext) {
         // Put initialization logic here.
         self.yaw = 180.0;
         // self.quit_button_handle =  create_quit_button(context.user_interfaces.first_mut());
-
     }
 
     fn on_start(&mut self, context: &mut ScriptContext) {
@@ -140,6 +188,7 @@ impl ScriptTrait for Player {
                         KeyCode::Space => {
                             self.jump = is_pressed;
                         }
+                        KeyCode::KeyF => self.fight = is_pressed,
                         _ => (),
                     }
                 }
@@ -148,98 +197,111 @@ impl ScriptTrait for Player {
         }
     }
 
-
-
     fn on_update(&mut self, ctx: &mut ScriptContext) {
         // Put object logic here.
+        let mbc = ctx.scene.graph.begin_multi_borrow();
         let mut look_vector = Vector3::default();
         let mut side_vector = Vector3::default();
         let mut jump_vector = Vector3::default();
-        if let Some(camera) = ctx.scene.graph.try_get_mut(self.camera) {
-            look_vector = camera.look_vector();
-            side_vector = camera.side_vector();
-            jump_vector = camera.up_vector();
+        let mut camera = mbc.try_get_mut(self.camera).unwrap();
+        look_vector = camera.look_vector();
+        side_vector = camera.side_vector();
+        jump_vector = camera.up_vector();
 
-            let yaw = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), self.yaw.to_radians());
-            let transform = camera.local_transform_mut();
-            // transform.set_rotation(
-            //     UnitQuaternion::from_axis_angle(
-            //         &UnitVector3::new_normalize(yaw * Vector3::x()),
-            //         self.pitch.to_radians(),
-            //     ) * yaw,
-            // );
+        let yaw = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), self.yaw.to_radians());
+        let transform = camera.local_transform_mut();
+        // transform.set_rotation(
+        //     UnitQuaternion::from_axis_angle(
+        //         &UnitVector3::new_normalize(yaw * Vector3::x()),
+        //         self.pitch.to_radians(),
+        //     ) * yaw,
+        // );
+
+        let mut rigid_body = mbc.try_get_component_of_type_mut::<RigidBody>(ctx.handle).unwrap();
+        // Form a new velocity vector that corresponds to the pressed buttons.
+        let mut velocity = Vector3::new(0.0, 0.0, 0.0);
+        let yaw = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), self.yaw.to_radians());
+
+        if self.move_forward {
+            velocity += look_vector;
+        }
+        if self.move_backward {
+            velocity -= look_vector;
+        }
+        if self.move_left {
+            velocity += side_vector;
+        }
+        if self.move_right {
+            velocity -= side_vector;
+        }
+        if self.jump {
+            velocity += jump_vector;
         }
 
-        if let Some(rigid_body) = ctx.scene.graph.try_get_mut_of_type::<RigidBody>(ctx.handle) {
-            // Form a new velocity vector that corresponds to the pressed buttons.
-            let mut velocity = Vector3::new(0.0, 0.0, 0.0);
-            let yaw = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), self.yaw.to_radians());
-            // let transform = rigid_body.local_transform_mut();
-            // transform.set_rotation(UnitQuaternion::from_axis_angle(
-            //     &UnitVector3::new_normalize(yaw * Vector3::x()),
-            //     self.pitch.to_radians(),
-            // ) * yaw,);
-            if self.move_forward {
-                println!("forward is pressed");
-                velocity += look_vector;
-            }
-            if self.move_backward {
-                velocity -= look_vector;
-            }
-            if self.move_left {
-                velocity += side_vector;
-            }
-            if self.move_right {
-                velocity -= side_vector;
-            }
-            if self.jump {
-                velocity += jump_vector;
-            }
-
-            let y_vel = rigid_body.lin_vel().y;
-            if let Some(normalized_velocity) = velocity.try_normalize(f32::EPSILON) {
-                let movement_speed = 240.0 * ctx.dt;
-                println!("dt: {}", ctx.dt);
-                rigid_body.set_lin_vel(Vector3::new(
-                    normalized_velocity.x * movement_speed,
-                    normalized_velocity.y * 3.0,
-                    normalized_velocity.z * movement_speed,
-                ));
-            } else {
-                // Hold player in-place in XZ plane when no button is pressed.
-                rigid_body.set_lin_vel(Vector3::new(0.0, y_vel, 0.0));
-            }
+        let y_vel = rigid_body.lin_vel().y;
+        if let Some(normalized_velocity) = velocity.try_normalize(f32::EPSILON) {
+            let movement_speed = 240.0 * ctx.dt;
+            rigid_body.set_lin_vel(Vector3::new(
+                normalized_velocity.x * movement_speed,
+                normalized_velocity.y * 3.0,
+                normalized_velocity.z * movement_speed,
+            ));
+        } else {
+            // Hold player in-place in XZ plane when no button is pressed.
+            rigid_body.set_lin_vel(Vector3::new(0.0, y_vel, 0.0));
         }
 
-        let model_transform = ctx
-            .scene
-            .graph
-            .try_get_mut(*self.model_root)
-            .map(|model| model.global_transform())
-            .unwrap_or_default();
-
-        // let mut velocity = Vector3::default();
-        if let Some(state_machine) = ctx
-            .scene
-            .graph
-            .try_get_mut(*self.absm)
-            .and_then(|node| node.query_component_mut::<AnimationBlendingStateMachine>())
-        {
-            let mut moving =
-                self.move_left || self.move_right || self.move_forward || self.move_backward;
-            if  self.jump {
-                moving = false
-            }
-            // println!("Moving: {}", moving);
-
-            let machine = state_machine.machine_mut().get_value_mut_silent();
-            // let val = state_machine.get;
-            // // val.get_field(name, func)
-            machine.set_parameter("walk", Parameter::Rule(moving));
-            machine.set_parameter("idle", Parameter::Rule(!moving));
-            // machine.set_parameter("attack", Parameter::Rule(self.jump));
-
-            // // val.set_parameter("idle",Parameter::Rule(!moving));
+        let mut moving =
+            self.move_left || self.move_right || self.move_forward || self.move_backward;
+        if self.jump {
+            moving = false
         }
+
+        let mut close_to_target = false;
+
+        let model_root = mbc.try_get(*self.model_root).unwrap();
+
+        println!("model root");
+        let target = mbc.try_get(self.target).unwrap();
+        println!("target");
+        let p = target
+            .global_position()
+            .metric_distance(&model_root.global_position());
+        // let p = 0.0;
+        close_to_target = p < 1.5;
+
+        let mut state_machine = mbc.try_get_component_of_type_mut::<AnimationBlendingStateMachine>(*self.absm).unwrap();
+        let machine = state_machine.machine_mut().get_value_mut_silent();
+        machine.set_parameter("walk", Parameter::Rule(moving));
+        machine.set_parameter("idle", Parameter::Rule(!moving));
+        machine.set_parameter("attack", Parameter::Rule(self.fight));
+
+        if close_to_target {
+            println!("CLOSE");
+            machine.set_parameter("attack", Parameter::Rule(true));
+        }
+
+        // // val.set_parameter("idle",Parameter::Rule(!moving));
+
+        let position = rigid_body.global_position();
+        let up_vector = rigid_body.up_vector();
+        let look_vector = rigid_body.look_vector();
+
+        // Update the viewing frustum.
+        self.update_frustum(position, look_vector, up_vector, 20.0);
+
+        // if self.target.is_none() {
+        //     for (handle, node) in ctx.scene.graph.pair_iter() {
+        //         if node.has_script::<Player>()
+        //             && self.frustum.is_contains_point(node.global_position())
+        //         {
+        //             self.target = handle;
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // A helper flag, that tells the bot that it is close enough to a target for melee
+        // attack.
     }
 }
